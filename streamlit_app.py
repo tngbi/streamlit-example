6c45e75603b1cc7f021654e3815f95b417a4e683
tngbi <153734758+tngbi@users.noreply.github.com>
2024-01-06 21:31:24 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ import hvplot.pandas  # Import hvPlot
 # Load the data
 @st.cache
 def load_data(year):
-    excel_file = ('/tngbi/streamlit-example/blob/master/sample_sales_team_data.xlsx')
+    excel_file = ('sample_sales_team_data.xlsx')
     return pd.read_excel(excel_file, sheet_name=str(year), index_col=0)
 
 data = load_data(2022)
